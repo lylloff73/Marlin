6862c3c70db04e73d9f8f51985cec5434fce5620
Jørn Lylloff <jorn.lylloff@gmail.com>
2024-02-18 01:59:32 +0100

    Disable case light. Enable part fan

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -750,7 +750,7 @@
  * @section caselight
  * M355 Case Light on-off / brightness
  */
-#define CASE_LIGHT_ENABLE
+//#define CASE_LIGHT_ENABLE
 #if ENABLED(CASE_LIGHT_ENABLE)
   //#define CASE_LIGHT_PIN 4                  // Override the default pin if needed
   #define INVERT_CASE_LIGHT false             // Set true if Case Light is ON when pin is LOW
